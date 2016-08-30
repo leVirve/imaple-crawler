252dbc06da59e460ba843f84e1bf6bc7fb687f07
Salas <gae.m.project@gmail.com>
2016-08-30 18:11:52 +0800

    integrate with `pyte`

--- main.py
+++ main.py
@@ -1,65 +1,71 @@
-import time
 import telnetlib
+import time
+import pyte
 import uao_decode
-from functools import partial
-
-
-def _encode(s, suffix):
-    return bytes(s + suffix, encoding='big5')
-
-encode_line = partial(_encode, suffix='\r\n')
-encode = partial(_encode, suffix='')
-
 
 tn = telnetlib.Telnet('imaple.tw')
+BOARD_NAME = 'nthu.course'
 
-expect_list = [
-    '您的帳號',
-    '● 請按任意鍵繼續 ●',
-    '再別楓橋，輕輕的我走了'
-]
+screen = pyte.Screen(80, 24)
+stream = pyte.Stream()
+screen.mode.discard(pyte.modes.LNM)
+stream.attach(screen)
 
-actions = [
-    'guest',
-    '',
-    'snthu.course'
-]
+class IMaple:
 
-expect_list = [encode(item) for item in expect_list]
-actions = [encode_line(item) for item in actions]
+    expect_list = [
+        '您的帳號',
+        '● 請按任意鍵繼續 ●',
+        '再別楓橋，輕輕的我走了'
+    ]
 
-while True:
-    idx, match, content = tn.expect(expect_list, 3)
+    actions = [
+        'guest',
+        '',
+        's%s' % BOARD_NAME
+    ]
 
-    if idx == -1:
-        break
 
-    # print(content.decode('uao_decode', 'ignore'))
-    # print(idx)
-    tn.write(actions[idx])
+def encode(s, suffix=''):
+    return bytes(s + suffix, encoding='big5')
 
-print(content.decode('uao_decode', 'ignore'))
+def enter_board():
+    expect_list = [encode(item) for item in IMaple.expect_list]
+    actions = [encode(item, '\r\n') for item in IMaple.actions]
+    while True:
+        idx, match, content = tn.expect(expect_list, 3)
+        if idx == -1:
+            break
+        tn.write(actions[idx])
 
-for i in range(13081, 13089):
-    print('Crawling article {}'.format(i))
-    tn.write(encode(str(i) + '\r\n' * 2))
-    time.sleep(1)
-    content = tn.read_very_eager().decode('uao_decode', 'ignore')
-    # 刪除沒清乾淨的文章列表
-    print(content)
 
-    counter = 2
-    while content.find('文章選讀') == -1:
-        print('\tdown {}...'.format(counter))
-        tn.write(encode('\x1B[B'))  # down
-        time.sleep(2)
-        _c = tn.read_very_eager().decode('uao_decode', 'ignore')
-        print(_c)
-        content += _c
-        counter += 1
+def get_posts(post_range):
+    for post_id in post_range:
+        print('Crawling article {}'.format(post_id))
 
-    with open('post-{}.txt'.format(13081), 'w', encoding='utf8') as f:
-        f.write(content)
-    tn.write(encode('q'))
+        tn.write(encode(str(post_id), '\r\n' * 2))
+        time.sleep(0.5)
 
-tn.close()
+        stream.feed(tn.read_very_eager().decode('uao_decode', 'ignore'))
+        lines = screen.display[:-1]
+
+        while True:
+            if screen.display[-1].find(' 文 章 選 讀') != -1:
+                break
+
+            tn.write(encode('\x1bOB'))  # next line
+            time.sleep(0.05)
+            stream.feed(tn.read_very_eager().decode('uao_decode', 'ignore'))
+            lines.append(screen.display[-2])
+
+
+        with open('post-{}.txt'.format(post_id), 'w', encoding='utf8') as f:
+            f.write('\n'.join(lines))
+
+        tn.write(encode('q'))
+
+
+if __name__ == '__main__':
+    enter_board()
+    get_posts(range(13081, 13085))
+    tn.close()

5c77f8ff3527600b822ab1d4144708226154883f
Salas <gae.m.project@gmail.com>
2016-08-30 19:16:22 +0800

    make it run with arguments

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
+import argparse
 import telnetlib
 import time
+import os
 import pyte
 import uao_decode
 
-tn = telnetlib.Telnet('imaple.tw')
-BOARD_NAME = 'nthu.course'
 
-screen = pyte.Screen(80, 24)
-stream = pyte.Stream()
-screen.mode.discard(pyte.modes.LNM)
-stream.attach(screen)
+def encode(s, suffix=''):
+    return bytes(s + suffix, encoding='big5')
+
 
 class IMaple:
 
-    expect_list = [
+    expects = [
         '您的帳號',
         '● 請按任意鍵繼續 ●',
         '再別楓橋，輕輕的我走了'
@@ -22,50 +21,75 @@ class IMaple:
     actions = [
         'guest',
         '',
-        's%s' % BOARD_NAME
+        's{BOARD_NAME}'
     ]
 
+    def __init__(self, host, board_name):
+        self.download_folder = 'posts'
+        self.board_name = board_name
+        self.client = telnetlib.Telnet(host)
+        self.screen = pyte.Screen(80, 24)
+        self.stream = pyte.Stream()
 
-def encode(s, suffix=''):
-    return bytes(s + suffix, encoding='big5')
+        self.screen.mode.discard(pyte.modes.LNM)
+        self.stream.attach(self.screen)
+        self.actions[-1] = 's{BOARD_NAME}'.format(BOARD_NAME=board_name)
+        self._lead_me_to_board()
 
-def enter_board():
-    expect_list = [encode(item) for item in IMaple.expect_list]
-    actions = [encode(item, '\r\n') for item in IMaple.actions]
-    while True:
-        idx, match, content = tn.expect(expect_list, 3)
-        if idx == -1:
-            break
-        tn.write(actions[idx])
-
-
-def get_posts(post_range):
-    for post_id in post_range:
-        print('Crawling article {}'.format(post_id))
-
-        tn.write(encode(str(post_id), '\r\n' * 2))
-        time.sleep(0.5)
-
-        stream.feed(tn.read_very_eager().decode('uao_decode', 'ignore'))
-        lines = screen.display[:-1]
-
+    def _lead_me_to_board(self):
+        expects = [encode(item) for item in self.expects]
+        actions = [encode(item, '\r\n') for item in self.actions]
         while True:
-            if screen.display[-1].find(' 文 章 選 讀') != -1:
+            idx, _, content = self.client.expect(expects, 3)
+            if idx == -1:
                 break
+            self.client.write(actions[idx])
 
-            tn.write(encode('\x1bOB'))  # next line
-            time.sleep(0.05)
-            stream.feed(tn.read_very_eager().decode('uao_decode', 'ignore'))
-            lines.append(screen.display[-2])
+    def get_posts(self, post_range):
+        board_dir = os.path.join(self.download_folder, self.board_name)
+        if not os.path.exists(board_dir):
+            os.makedirs(board_dir)
 
+        for post_id in post_range:
+            print('Crawling post#{}'.format(post_id))
+            self.client.write(encode(str(post_id), '\r\n' * 2))
+            time.sleep(0.5)
+            self._update_partial_content()
+            lines = self.screen.display[:-1]
 
-        with open('post-{}.txt'.format(post_id), 'w', encoding='utf8') as f:
-            f.write('\n'.join(lines))
+            while True:
+                if self.screen.display[-1].find(' 文 章 選 讀') != -1:
+                    break
+                self.client.write(encode('\x1bOB'))  # next line
+                time.sleep(0.05)
+                self._update_partial_content()
+                lines.append(self.screen.display[-2])
+            self.client.write(encode('q'))
 
-        tn.write(encode('q'))
+            self.save_post(lines, board_dir, post_id)
+
+    def _update_partial_content(self):
+        content = self.client.read_very_eager().decode('uao_decode', 'ignore')
+        self.stream.feed(content)
+
+    def save_post(self, lines, folder, post_id):
+        path = os.path.join(folder, 'post-{}.txt'.format(post_id))
+        content = '\n'.join(lines)
+        with open(path, 'w', encoding='utf8') as f:
+            f.write(content)
+
+    def close(self):
+        self.client.close()
 
 
 if __name__ == '__main__':
-    enter_board()
-    get_posts(range(13081, 13085))
-    tn.close()
+    parser = argparse.ArgumentParser(description='iMaple Crawler')
+    parser.add_argument('board')
+    parser.add_argument('start_id', type=int)
+    parser.add_argument('end_id' , type=int)
+    args = parser.parse_args()
+
+    #'nthu.course'
+    imaple = IMaple('imaple.tw', args.board)
+    imaple.get_posts(range(args.start_id, args.end_id))
+    imaple.close()
